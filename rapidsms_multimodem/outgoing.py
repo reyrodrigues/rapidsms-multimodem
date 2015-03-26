b13a553b02e98edcba0f5133095d6220aa9b11d1
Vinod Kurup <vinod@kurup.com>
2015-03-26 14:41:44 -0400

    Get language encoding and url encoding working

--- rapidsms_multimodem/outgoing.py
+++ rapidsms_multimodem/outgoing.py
@@ -1,14 +1,16 @@
-import copy
 import logging
 import requests
 import collections
 import pprint
-import urllib
 
 from rapidsms.backends.base import BackendBase
 
+from .utils import unicode_to_ismsformat, isms_urlencode
 
 logger = logging.getLogger(__name__)
+
+ISMS_ASCII = 0
+ISMS_UNICODE = 2
 
 
 class MultiModemBackend(BackendBase):
@@ -30,12 +32,23 @@ class MultiModemBackend(BackendBase):
         params['user'] = self.sendsms_user
         params['passwd'] = self.sendsms_pass
         params['cat'] = 1
-        params['enc'] = self.sendsms_params.get('enc', 0)
+        params['enc'] = ISMS_ASCII
         params['to'] = to
-        # TODO: figure out how to make SMS %20 space delineated instead of +
-        #       otherwise SMS is delivered as this+contains+spaces
-        #       maybe use unicode encoding (enc) above?
-        params['text'] = text
+        # 'text' is tricky. iSMS has 3 encodings: ascii, enhanced ascii, and 'unicode'
+        # (Note: it's not really Unicode, but a iSMS custom binary format)
+        # we can't just use 'unicode' for everything because each encoding has character limits.
+        # ascii = 160, enhanced_ascii = 140, unicode = 70
+        # instead we'll check if it's ascii first, and use 'unicode' if it's not.
+        # This code doesn't support 'enhanced ascii' at all.
+        try:
+            text.encode('ascii')
+        except UnicodeEncodeError:
+            params['enc'] = ISMS_UNICODE
+
+        if params['enc'] == ISMS_ASCII:
+            params['text'] = text
+        else:
+            params['text'] = unicode_to_ismsformat(text)
         params.update(self.sendsms_params)
         kwargs['params'] = params
         return kwargs
@@ -43,8 +56,9 @@ class MultiModemBackend(BackendBase):
     def send(self, id_, text, identities, context={}):
         logger.debug('Sending message: %s' % text)
         kwargs = self.prepare_request(id_, text, identities, context)
-        logger.debug('params: %s' % pprint.pformat(kwargs["params"]))
-        r = requests.get(**kwargs)
+        logger.warning('params: %s' % pprint.pformat(kwargs["params"]))
+        params = isms_urlencode(kwargs['params'])
+        r = requests.get(url=kwargs['url'], params=params)
         if r.status_code != requests.codes.ok:
             r.raise_for_status()
         if "Err" in r.text:
---
+++ rapidsms_multimodem/utils.py
@@ -0,0 +1,54 @@
+import urllib
+
+
+def unicode_to_ismsformat(unicode_str):
+    """
+    Example:
+    text_to_ismsformat('The quick brown fox settled his disputes out of court.')
+    ->  "0054;0068;0065;0020;0071;0075;0069;0063;006B;0020;0062;0072;006F;0077;
+         006E;0020;0066;006F;0078;0020;0073;0065;0074;0074;006C;0065;0064;0020;
+         0068;0069;0073;0020;0064;0069;0073;0070;0075;0074;0065;0073;0020;006F;
+         0075;0074;0020;006F;0066;0020;0063;006F;0075;0072;0074;002E;"
+    """
+    encoded = unicode(unicode_str).encode('utf-16be')
+    encoded_array = bytearray(encoded)
+    byte_str = ""
+    for i in xrange(0, len(encoded_array), 2):
+        byte_str += "%02x%02x;" % (encoded_array[i], encoded_array[i+1])
+    return byte_str
+
+
+def ismsformat_to_unicode(byte_str):
+    """
+    Reverses the formatting performed by unicode_to_ismsformat.
+    """
+    unformatted = byte_str.replace(';', '')
+    unicode_str = unicode()
+    if (len(unformatted) % 4) == 0:
+        for i in xrange(0, len(unformatted), 4):
+            unicode_str += unichr(int(unformatted[i] +
+                                      unformatted[i + 1] +
+                                      unformatted[i + 2] +
+                                      unformatted[i + 3], 16))
+    elif (len(unformatted) % 2) == 0:
+        for i in xrange(0, len(unformatted), 2):
+            unicode_str += unichr(int(unformatted[i] +
+                                      unformatted[i + 1], 16))
+    return unicode_str
+
+
+def isms_urlencode(self, query):
+    """Encode a sequence of two-element tuples into a URL query string.
+
+    Customized because `text` parameter must have space characters quoted with %20 instead of +
+    """
+    parts = []
+    for k, v in query.items():
+        k = urllib.quote_plus(str(k))
+        # text keyword must be quoted with spaces converted to '%20', not '+'
+        if k == 'text':
+            v = urllib.quote(str(v))
+        else:
+            v = urllib.quote_plus(str(v))
+        parts.append(k + '=' + v)
+    return '&'.join(parts)
