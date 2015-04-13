9f00d1059d7397cc4c11ff2f84a9ef9c01d09a43
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2015-04-13 11:55:42 +0300

    Refactoring incoming view

--- rapidsms_multimodem/tests.py
+++ rapidsms_multimodem/tests.py
@@ -7,7 +7,56 @@ from rapidsms.tests.harness import RapidTest
 from rapidsms_multimodem.views import MultiModemView
 
 
-VALID_POST = """<?xml version="1.0" encoding="ISO-8859-1"?>
+VALID_POST = """
+<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>
+<Response>
+    <Msg_Count>1</Msg_Count>
+    <MessageNotification>
+        <Message_Index>1</Message_Index>
+        <ModemNumber>2:111222333</ModemNumber>
+        <SenderNumber>+222333444</SenderNumber>
+        <Date>15/04/13</Date>
+        <Time>10:54:27</Time>
+        <EncodingFlag>ASCII</EncodingFlag>
+        <Message>Test</Message>
+    </MessageNotification>
+</Response>
+<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>
+<Response>
+    <Msg_Count>2</Msg_Count>
+    <MessageNotification>
+        <Message_Index>1</Message_Index>
+        <ModemNumber>2:380665717968</ModemNumber>
+        <SenderNumber>+380990405272</SenderNumber>
+        <Date>15/04/13</Date>
+        <Time>10:55:58</Time>
+        <EncodingFlag>ASCII</EncodingFlag>
+        <Message>Testn2</Message>
+    </MessageNotification>
+        <MessageNotification>
+        <Message_Index>2</Message_Index>
+        <ModemNumber>2:380665717968</ModemNumber>
+        <SenderNumber>+380990405272</SenderNumber>
+        <Date>15/04/13</Date>
+        <Time>10:58:39</Time>
+        <EncodingFlag>Unicode</EncodingFlag>
+        <Message>0429043D043F0437043D043C0433043C0436043D043C</Message>
+    </MessageNotification>
+</Response>
+<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>
+<Response>
+    <Msg_Count>1</Msg_Count>
+    <MessageNotification>
+        <Message_Index>1</Message_Index>
+        <ModemNumber>2:380665717968</ModemNumber>
+        <SenderNumber>+380990405272</SenderNumber>
+        <Date>15/04/13</Date>
+        <Time>11:01:16</Time>
+        <EncodingFlag>Unicode</EncodingFlag>
+        <Message>00C90020006F002000660069006D00200064006F0020006D0075006E0064006F002C002000650020006E00E3006F0020006D006500200069006D0070006F007200740061002E0020</Message>
+    </MessageNotification>
+</Response>
+<?xml version="1.0" encoding="ISO-8859-1"?>
 <Response>
 <MessageNotification>
 <ModemNumber>2:19525945092</ModemNumber>
